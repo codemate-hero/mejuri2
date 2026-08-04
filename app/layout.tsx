ff8232b98be2657fb51a2710e52700a7720c72e7
Hamad <hamadmemon2314@gmail.com>
2026-08-05 00:02:14 +0500

    google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,25 @@ export const metadata: Metadata = {
       className={`${inter.variable} antialiased`}
     >
       <body suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W56MJPLG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <NavigationRecovery />
         {children}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W56MJPLG');
+          `}
+        </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-VYNN5LPC4V"
           strategy="afterInteractive"
